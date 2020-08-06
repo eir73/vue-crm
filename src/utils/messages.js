58ea47d1534030a95d20269569a9d7c31c922cb9
eir73 <roflaneir73@gmail.com>
2020-08-06 23:00:00 +0300

    add login/registration

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
-    'logout': 'Вы вышли из системы',
+    'logout' : 'Вы вышли из системы',
+    'auth/wrong-password' : 'Неправильный пароль',
+    'auth/user-not-found' : 'Пользователя с таким email не существует',
+    'auth/email-already-in-use' : 'Уже есть аккаунт с таким email'
 }
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -98,7 +98,7 @@ export default {
     agree: {checked: v => v}
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if(this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -110,7 +110,10 @@ export default {
         name: this.name
       }
 
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('register', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
