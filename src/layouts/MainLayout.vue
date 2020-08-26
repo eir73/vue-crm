f984d72b4ffc77fdd915671da69a01b46a7cd6c0
eir73 <roflaneir73@gmail.com>
2020-08-26 12:48:09 +0300

    created routes protection (router/index.js)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,7 +13,11 @@
     </main>
 
     <div class="fixed-action-btn">
-      <router-link class="btn-floating btn-large blue" to="/record">
+      <router-link 
+        class="btn-floating btn-large blue" 
+        to="/record"
+        v-tooltip="'Добавить запись'"
+        data-position="left">
         <i class="large material-icons">add</i>
       </router-link>
     </div>
@@ -24,6 +28,7 @@
 <script>
 import Navbar from "@/components/app/Navbar.vue";
 import Sidebar from "@/components/app/Sidebar.vue";
+import messages from '../utils/messages'
 
 export default {
   name: "main-layout",
@@ -37,6 +42,16 @@ export default {
     } 
 
     this.loading = false
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(fbError) {
+      this.$error(messages[fbError.code] || 'Что-то пошло не так')
+    }
   },
   components: {
     Navbar,
