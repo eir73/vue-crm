e0144d98b26313612aae239ace0084d40e458561
eir73 <roflaneir73@gmail.com>
2020-08-28 12:04:25 +0300

    enabled name change in profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,11 +4,28 @@
       <h3>Профиль</h3>
     </div>
 
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
-        <input id="description" type="text" />
-        <label for="description">Имя</label>
-        <span class="helper-text invalid">name</span>
+        <input 
+          id="name" 
+          type="text" 
+          v-model.trim="name"
+          :class="{invalid: $v.name.$dirty && !$v.name.required}"
+        />
+        <label for="name">Имя</label>
+        <small 
+          class="helper-text invalid"
+          v-if="$v.name.$dirty && !$v.name.required"
+        >Введите имя</small>
+      </div>
+
+      <div class="switch">
+        <label>
+          English
+          <input type="checkbox" v-model="isRuLocale">
+          <span class="lever"></span>
+          Русский
+        </label>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -18,3 +35,50 @@
     </form>
   </div>
 </template>
+
+<style scoped>
+  .switch {
+    margin-bottom: 2rem;
+  }
+</style>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+import {required} from 'vuelidate/lib/validators' 
+
+export default {
+  data: () => ({
+    name: '',
+    isRuLocale: true
+  }),
+  validations: {
+    name: { required }
+  },
+  mounted() {
+    this.name = this.info.name
+    this.isRuLocale = this.info.locale === 'ru'
+    setTimeout(M.updateTextFields, 0)
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler() {
+      if(this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+      try {
+        await this.updateInfo({
+          name: this.name,
+          locale: this.isRuLocale ? 'ru' : 'en'
+        })
+      } catch (e) {
+        throw e
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['info'])
+  }
+}
+</script>
