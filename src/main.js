a1aa9f413fa841a7969b8e0a94cd35a15cd5f845
eir73 <roflaneir73@gmail.com>
2020-09-06 13:42:22 +0300

    deploy on firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import Paginate from 'vuejs-paginate'
+import VueMeta from 'vue-meta'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -8,6 +9,7 @@ import dateFilter from './filters/date.filter'
 import localizeFilter from './filters/localize.filter'
 import currencyFilter from './filters/currency.filter'
 import messagePlugin from './utils/message.plugin'
+import titlePlugin from './utils/title.plugin'
 import tooltipDirective from './directives/tooltip.directive'
 import Loader from './components/app/Loader'
 import './registerServiceWorker'
@@ -19,8 +21,10 @@ import 'firebase/database'
 
 Vue.config.productionTip = false
 
-Vue.use(messagePlugin)
+Vue.use(VueMeta)
 Vue.use(Vuelidate)
+Vue.use(titlePlugin)
+Vue.use(messagePlugin)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 Vue.filter('localize', localizeFilter)
